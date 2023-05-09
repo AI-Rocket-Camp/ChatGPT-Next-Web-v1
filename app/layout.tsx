dc06bfdb8f78e6c92be88c2c100d5e59363ac725
AI-Rocket-Camp <50973253+AI-Rocket-Camp@users.noreply.github.com>
2023-05-09 23:10:57 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "AI火箭营Chat Service",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
     title: "AI火箭营Chat Service",
